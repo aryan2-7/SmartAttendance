43ee5242e878af0f3296394f03c0fe168ac448b8
Aryan Khatri <aryan2007bro@gmail.com>
2026-05-27 13:24:20 +0545

    Create FaceRegistration.h

---
+++ src/registration/FaceRegistration.h
@@ -0,0 +1,7 @@
+// Header file for the face registration process
+//
+// This file will contain the FaceRegistration class which will do the following:
+//  1.Initialise the camera and show a prewview
+//  2.Use haar cascade to detect a face in frame
+//  3.Take 30 pictures, of the face convert it into greyscale
+//  4.Train an LBPH recogni
