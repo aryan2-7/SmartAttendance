5baa705191cec1b62ecbe7f87af6dddc157ee6b4
Aryan Khatri <aryan9725@student.ku.edu.np>
2026-08-05 13:56:43 +0545

    Added git lfs to track the sqlite3 files

---
+++ sqlite/sqlite3.c
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:9ac97192c93a6d9671d2ea667ba14e3c8fe3719a171afc5827c9d86d0278fc14
+size 9489774
---
+++ sqlite/sqlite3.h
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:16d6e2f265fb13ece42329da39a865d4c540045b444d16b4bf9f7e1a7e39408f
+size 690047
